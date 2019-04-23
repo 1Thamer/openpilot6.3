64ce189c3b3f62551982fecc252dc979ef6cb17b
Shane <shane@smiskol.com>
2019-04-23 13:44:07 -0500

    updates

--- selfdrive/phantom_receiver.py
+++ selfdrive/phantom_receiver.py
@@ -3,9 +3,8 @@ import json
 phantom_file = "/data/phantom.json"
 
 def write_file(status, speed, angle, time):
-  status = True if status.lower() == "true" else False
   with open(phantom_file, "w") as phantom:
-    json.dump({"status": status, "speed": speed, "angle": angle, "time": time}, phantom)
+    json.dump({"status": bool(status), "speed": speed, "angle": angle, "time": time}, phantom)
 
 if __name__ == "__main__":
   write_file(str(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]))
