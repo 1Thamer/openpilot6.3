ddff81f9bf9bba6559af93315f39718518506a20
Shane <shane@smiskol.com>
2019-04-25 08:19:38 -0500

    kegman fix

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -6,19 +6,12 @@ import time
 from selfdrive.swaglog import cloudlog
 from common.basedir import BASEDIR
 
-thread_counter = 0  # don't change
-thread_timeout = 5.0  # minutes to wait before stopping thread. reading or writing will reset the counter
-thread_interval = 30.0  # seconds to sleep between checks
-thread_started = False
-kegman_file = "/data/kegman.json"
-variables_written = []
-
 def read_config():
-  default_config = {"cameraOffset": "0.06", "lastTrMode": "1", "battChargeMin": "90", "battChargeMax": "95",
-                    "wheelTouchSeconds": "1800", "battPercOff": "25", "carVoltageMinEonShutdown": "11200",
-                    "brakeStoppingTarget": "0.25", "angle_steers_offset": "0", "brake_distance_extra": "1",
-                    "lastALCAMode": "1", "brakefactor": "1.2", "lastGasMode": "0", "lastSloMode": "1",
-                    "leadDistance": "5"}
+  default_config = {"cameraOffset": 0.06, "lastTrMode": 1, "battChargeMin": 90, "battChargeMax": 95,
+                    "wheelTouchSeconds": 1800, "battPercOff": 25, "carVoltageMinEonShutdown": 11200,
+                    "brakeStoppingTarget": 0.25, "angle_steers_offset": 0, "brake_distance_extra": 1,
+                    "lastALCAMode": 1, "brakefactor": 1.2, "lastGasMode": 0, "lastSloMode": 1,
+                    "leadDistance": 5}
 
   if os.path.isfile(kegman_file):
     with open(kegman_file, "r") as f:
@@ -28,42 +21,41 @@ def read_config():
         cloudlog.exception("reading kegman.json error")
         config = default_config
     if "battPercOff" not in config:
-      config.update({"battPercOff": "25"})
+      config.update({"battPercOff": 25})
     if "carVoltageMinEonShutdown" not in config:
-      config.update({"carVoltageMinEonShutdown": "11200"})
+      config.update({"carVoltageMinEonShutdown": 11200})
     if "brakeStoppingTarget" not in config:
-      config.update({"brakeStoppingTarget": "0.25"})
+      config.update({"brakeStoppingTarget": 0.25})
     if "angle_steers_offset" not in config:
-      config.update({"angle_steers_offset": "0"})
+      config.update({"angle_steers_offset": 0})
     if "brake_distance_extra" not in config:  # extra braking distance in m
-      config.update({"brake_distance_extra": "1"})
+      config.update({"brake_distance_extra": 1})
     if "lastALCAMode" not in config:
-      config.update({"lastALCAMode": "1"})
+      config.update({"lastALCAMode": 1})
     if "brakefactor" not in config:  # brake at 20% higher speeds than what I like
-      config.update({"brakefactor": "1.2"})
+      config.update({"brakefactor": 1.2})
     if "lastGasMode" not in config:
-      config.update({"lastGasMode": "0"})
+      config.update({"lastGasMode": 0})
     if "lastSloMode" not in config:
-      config.update({"lastSloMode": "1"})
+      config.update({"lastSloMode": 1})
     if "leadDistance" not in config:  # leadDistance only works for Accord and Insight, have not tested other honda vehicles
-      config.update({"leadDistance": "5.0"})
+      config.update({"leadDistance": 5.0})
 
     # force update
     if config["carVoltageMinEonShutdown"] == "11800":
-      config.update({"carVoltageMinEonShutdown": "11200"})
+      config.update({"carVoltageMinEonShutdown": 11200})
     if int(config["wheelTouchSeconds"]) < 200:
-      config.update({"wheelTouchSeconds": "1800"})
+      config.update({"wheelTouchSeconds": 1800})
     if int(config["battChargeMin"]) == 85:
-      config.update({"battChargeMin": "90"})
+      config.update({"battChargeMin": 90})
     if int(config["battChargeMax"]) == 90:
-      config.update({"battChargeMax": "95"})
+      config.update({"battChargeMax": 95})
   else:
+    write_config(default_config)
     config = default_config
   return config
 
-conf = read_config()
-
-def kegman_thread(t_i=thread_interval):  # read and write thread; now merges changes from file and variable
+def kegman_thread():  # read and write thread; now merges changes from file and variable
   global conf
   global thread_counter
   global variables_written
@@ -72,7 +64,7 @@ def kegman_thread(t_i=thread_interval):  # read and write thread; now merges cha
   try:
     while True:
       thread_counter += 1
-      time.sleep(t_i)  # every n seconds check for conf change
+      time.sleep(thread_interval)  # every n seconds check for conf change
       with open(kegman_file, "r") as f:
         conf_tmp = json.load(f)
       if conf != last_conf or conf != conf_tmp:  # if either variable or file has changed
@@ -98,7 +90,7 @@ def kegman_thread(t_i=thread_interval):  # read and write thread; now merges cha
         return
   except:
     print("Error in kegman thread!")
-    cloudlog.exception("error in kegman thread")
+    cloudlog.warning("error in kegman thread")
     thread_started = False
 
 def write_config(conf):  # never to be called outside kegman_conf
@@ -122,9 +114,9 @@ def save(data):  # allows for writing multiple key/value pairs
     variables_written.append(key)
   conf.update(data)
 
-def get(key_s=None):  # can get multiple keys from a list
+def get(key_s=""):  # can get multiple keys from a list
   global thread_counter
-  if key_s == None:  # get all
+  if key_s == "":  # get all
     return conf
   else:
     thread_counter = 0
@@ -135,9 +127,10 @@ def get(key_s=None):  # can get multiple keys from a list
     else:
       return None
 
-def start_thread(t_i=thread_interval):  # call if file needs to read live updates from kegman.json but not write
-  global thread_started
-  if not thread_started and BASEDIR == "/data/openpilot":
-    threading.Thread(target=kegman_thread, args=(t_i,)).start()  # automatically start write thread if file needs it
-    thread_started = True
-    print("Starting thread!")
+thread_counter = 0  # don't change
+thread_timeout = 5.0  # minutes to wait before stopping thread. reading or writing will reset the counter
+thread_interval = 30.0  # seconds to sleep between checks
+thread_started = False
+kegman_file = "/data/kegman.json"
+variables_written = []
+conf = read_config()
