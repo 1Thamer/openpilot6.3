87a32f877b83a0f40fa1913481a49c93444a7421
Shane <shane@smiskol.com>
2019-04-28 18:10:17 -0500

    optimize phantom

--- selfdrive/phantom.py
+++ selfdrive/phantom.py
@@ -5,10 +5,14 @@ class Phantom():
     self.data = None
     self.update()
     self.phantom_file = "/data/phantom.json"
+    self.frames = 0
 
   def update(self):
-    try:
-      with open(self.phantom_file, "r") as p_f:
-        self.data = json.load(p_f)
-    except:
-      self.data = {"status": False}
+    self.frames += 1
+    if self.frames > 200:
+      self.frames = 0
+      try:
+        with open(self.phantom_file, "r") as p_f:
+          self.data = json.load(p_f)
+      except:
+        self.data = {"status": False}
