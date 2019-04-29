98f1e3397495f24ddf423b3b07747ad8ddf3e717
Shane <shane@smiskol.com>
2019-04-28 22:14:16 -0500

    if car is on for more than 20 minutes without phantom activation, shut down thread

--- selfdrive/phantom.py
+++ selfdrive/phantom.py
@@ -6,11 +6,13 @@ def phantom_thread():
   global data
   global high_frequency
   thread_interval = 5  # 12hz
+  thread_start = time.time()
   while True:
     time.sleep(thread_interval)
     start = time.time()
     data = read_phantom()
     if data["status"]:
+      thread_start = time.time()
       if high_frequency:
         thread_interval = 0.02  # 50hz for steering
       else:
@@ -18,6 +20,8 @@ def phantom_thread():
     else:
       thread_interval = 5
     thread_interval = max(thread_interval - (time.time() - start), 0)  # rate keeper
+    if (time.time() - thread_start) > (20*60):  # if car is on for more than 20 minutes without phantom activation, shut down thread
+      break
 
 def read_phantom():
   try:
