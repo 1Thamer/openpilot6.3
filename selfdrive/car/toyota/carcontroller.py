5f4a03a0839327885e3cbc3243eae11fdc2ea0ab
Shane <shane@smiskol.com>
2019-05-08 17:47:45 -0500

    remove phantom from latcontrol

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -140,7 +140,7 @@ class CarController(object):
     self.last_fault_frame = -200
     self.blindspot_debug_enabled_left = False
     self.blindspot_debug_enabled_right = False
-    self.phantom = Phantom(timeout=False)
+    self.phantom = Phantom(timeout=False)  # no timeout for steering
 
     self.fake_ecus = set()
     if enable_camera: self.fake_ecus.add(ECU.CAM)
