d41f8f42fd8a7a91ff7fb43dbc87056a3236c73d
Arne Schwarck <arneschwarck@gmail.com>
2019-05-09 22:46:25 +0200

    rotate in the right direction

--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -1268,7 +1268,7 @@ static void ui_draw_vision_event(UIState *s) {
     }
     nvgSave(s->vg);
     nvgTranslate(s->vg,bg_wheel_x,(bg_wheel_y + (bdr_s*1.5)));
-    nvgRotate(s->vg,img_rotation);
+    nvgRotate(s->vg,-img_rotation);
     nvgBeginPath(s->vg);
     NVGpaint imgPaint = nvgImagePattern(s->vg, img_wheel_x-bg_wheel_x, img_wheel_y-(bg_wheel_y + (bdr_s*1.5)),
       img_wheel_size, img_wheel_size, 0, s->img_wheel, img_wheel_alpha);
