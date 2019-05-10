d9d35daca92733e425356f67397a270c72f3f79f
Shane <shane@smiskol.com>
2019-05-10 09:28:09 -0500

    sync

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -17,7 +17,7 @@ AudibleAlert = car.CarControl.HUDControl.AudibleAlert
 # Accel limits
 ACCEL_HYST_GAP = 0.02  # don't change accel command for small oscilalitons within this value
 ACCEL_MAX = 3.5  # 3.5 m/s2
-ACCEL_MIN = -3.0 # 4   m/s2
+ACCEL_MIN = -4.0 # 4   m/s2
 ACCEL_SCALE = max(ACCEL_MAX, -ACCEL_MIN)
 
 # Steer torque limits
