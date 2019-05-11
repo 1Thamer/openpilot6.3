22d2e35feda81c3e4ce4c6a2f90ffbd5a17c68fb
Arne Schwarck <arneschwarck@gmail.com>
2019-05-11 22:24:38 +0200

    Update version.h

--- selfdrive/common/version.h
+++ selfdrive/common/version.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#define COMMA_VERSION "0.5.10.8-arne182"
-=======
-#define COMMA_VERSION "0.5.11-release"
->>>>>>> d1866845df423c6855e2b365ff230cf7d89a420b
+#define COMMA_VERSION "0.5.11.1-arne182"
