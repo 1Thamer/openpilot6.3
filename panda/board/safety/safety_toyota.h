be58f889b62e6899c05273d0db4e5c675cf5c411
Arne Schwarck <arneschwarck@gmail.com>
2019-05-12 20:37:54 +0200

    Update safety_toyota.h

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -8,7 +8,7 @@ const int TOYOTA_MAX_TORQUE = 1500;       // max torque cmd allowed ever rav4 st
 // packet is sent at 100hz, so this limit is 1000/sec
 const int TOYOTA_MAX_RATE_UP = 10;        // ramp up slow
 const int TOYOTA_MAX_RATE_DOWN = 25;      // ramp down fast
-const int TOYOTA_MAX_TORQUE_ERROR = 8350;  // max torque cmd in excess of torque motor
+const int TOYOTA_MAX_TORQUE_ERROR = 1350;  // max torque cmd in excess of torque motor
 
 // real time torque limit to prevent controls spamming
 // the real time limit is 1500/sec
