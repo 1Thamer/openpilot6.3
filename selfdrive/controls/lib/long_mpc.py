36d03f5ada39995e654de993648a70e30aa6ded3
Shane <shane@smiskol.com>
2019-05-27 13:34:29 -0500

    add dynamic follow

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -1,5 +1,5 @@
+from common.numpy_fast import interp
 import numpy as np
-
 import selfdrive.messaging as messaging
 from selfdrive.swaglog import cloudlog
 from common.realtime import sec_since_boot
@@ -62,6 +62,55 @@ class LongitudinalMpc(object):
     self.cur_state[0].v_ego = v
     self.cur_state[0].a_ego = a
 
+  def dynamic_follow(self, velocity):  # in m/s
+    x_vel = [0.0, 1.86267, 3.72533, 5.588, 7.45067, 9.31333, 11.55978, 13.645, 22.352, 31.2928, 33.528, 35.7632, 40.2336]  # velocity
+    y_mod = [1.03, 1.05363, 1.07879, 1.11493, 1.16969, 1.25071, 1.36325, 1.43, 1.6, 1.7, 1.75618, 1.85, 2.0]  # distances
+
+    TR = interp(self.v_ego, x_vel, y_mod)
+    return TR
+
+    stop_and_go_magic_number = 8.9408  # 20 mph
+
+    if velocity <= 0.44704:  # 1 mph
+      self.stop_and_go = True
+    elif velocity >= stop_and_go_magic_number:
+      self.stop_and_go = False
+
+    if self.stop_and_go:  # this allows a smooth deceleration to a stop, while being able to have smooth stop and go
+      x = [stop_and_go_magic_number / 2.0, stop_and_go_magic_number]  # from 10 to 20 mph, ramp 1.8 sng distance to regular dynamic follow value
+      y = [1.8, interp(stop_and_go_magic_number, x_vel, y_mod)]
+      TR = interp(velocity, x, y)
+    else:
+      TR = interpolate.interp1d(x_vel, y_mod, fill_value='extrapolate')(velocity)[()]  # extrapolate above 90 mph
+
+    if self.relative_velocity is not None:
+      x = [-15.6464, -11.6231, -7.8428, -5.45, -4.3701, -3.2187, -1.7241, -0.911, -0.4917, 0.0, 0.2682, 0.775, 1.8532, 2.6851]  # relative velocity values
+      y = [0.504, 0.465, 0.4009, 0.3226, 0.2852, 0.2184, 0.168, 0.1187, 0.0683, 0, -0.0554, -0.1371, -0.2402, -0.3004]  # modification values
+      TR_mod = interp(self.relative_velocity, x, y)  # factor in lead relative velocity
+
+      x = [-4.4704, -2.2352, -0.8941, 0.0, 1.3411]   # self acceleration values
+      y = [0.158, 0.058, 0.016, 0, -0.13]  # modification values
+      TR_mod += interp(self.get_acceleration(self.dynamic_follow_dict["self_vels"], True), x, y)  # factor in self acceleration
+
+      x = [-4.49033, -1.87397, -0.66245, -0.26291, 0.0, 0.5588, 1.34112]  # lead acceleration values
+      y = [0.37909, 0.30045, 0.20378, 0.04158, 0, -0.115, -0.195]  # modification values
+      TR_mod += interp(self.get_acceleration(self.dynamic_follow_dict["lead_vels"], False), x, y)  # factor in lead car's acceleration; should perform better
+
+      x = [0, 6.9128, 16.0047, 27.163, 37.6085, 50.3843, 54.6429, 65.3908, 83.0336, 93.1731]  # distance in meters
+      y = [1.0175, 1.0079, 1.0045, 1.0083, 1.0176, 1.0547, 1.0911, 1.1454, 1.1838, 1.195]
+      TR_mod *= interp(self.relative_distance, x, y)  # factor in distance from lead car to try and brake quicker
+
+      x = [1.1594, 2.7298, 6.1562, 10.5105, 22.352, 33.528]  # speed in m/s
+      y = [0.7, 0.885, 1.0, 1.024, 1.0, 0.9]
+      TR_mod *= float(interp(velocity, x, y))  # lower TR modification for stop and go, and at higher speeds
+
+      TR += TR_mod
+      TR *= self.get_traffic_level(self.dynamic_follow_dict["traffic_vels"])  # modify TR based on last minute of traffic data
+    if TR < 0.65:
+      return 0.65
+    else:
+      return round(TR, 4)
+
   def dynamic_follow(self):
     return 1.8
 
@@ -74,17 +123,18 @@ class LongitudinalMpc(object):
       if self.last_cost != 1.0:
         self.libmpc.change_tr(MPC_COST_LONG.TTC, 1.0, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
         self.last_cost = 1.0
-      return 1.2  # accelerate for lane change
+      return 0.9  # accelerate for lane change
     elif read_distance_lines == 1:
       if self.last_cost != 1.0:
         self.libmpc.change_tr(MPC_COST_LONG.TTC, 1.0, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
         self.last_cost = 1.0
       return 0.9  # 10m at 40km/hr
     elif read_distance_lines == 2:
+      TR = self.dynamic_follow()
       if self.last_cost != 0.1:
         self.libmpc.change_tr(MPC_COST_LONG.TTC, 0.1, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
         self.last_cost = 0.1
-      return 1.8
+      return TR
     else:
       if self.last_cost != 0.05:
         self.libmpc.change_tr(MPC_COST_LONG.TTC, 0.05, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
