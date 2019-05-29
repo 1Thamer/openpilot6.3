02ed092b78e03a65b56531c5a7362a14a7b6a417
Arne Schwarck <arneschwarck@gmail.com>
2019-05-29 11:51:49 +0200

    Try to add alca support back for prius

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -92,7 +92,10 @@ class CarInterface(object):
       ret.steerRatio = 15.00   # unknown end-to-end spec
       tire_stiffness_factor = 0.6371   # hand-tune
       ret.mass = 3045 * CV.LB_TO_KG + std_cargo
-
+      
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4], [0.05]]
+      ret.lateralTuning.pid.kf = 0.0001 # full torque for 10 deg at 80mph means 0.00007818594
+      
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGain = 4.0
       ret.lateralTuning.indi.outerLoopGain = 3.0
