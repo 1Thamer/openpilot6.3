dab541ec212b6a77d1e53090ac2e9f573adf36a0
Shane <shane@smiskol.com>
2019-06-01 06:52:16 -0500

    test

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -80,7 +80,7 @@ def fingerprint(logcan, timeout):
   st_passive = sec_since_boot()  # only relevant when passive
   can_seen = False
   while 1:
-    with open("/data/my_fingerprint", "w") as f:
+    with open("/data/my_fingerprint", "a") as f:
       f.write(str("in loop\n"))
     for a in messaging.drain_sock(logcan):
       for can in a.can:
