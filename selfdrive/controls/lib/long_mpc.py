b759b44d06c83eee73dcd8e9d8af9db353c0f56b
Arne Schwarck <arneschwarck@gmail.com>
2019-06-03 09:53:04 +0200

    Fix cost

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -160,7 +160,7 @@ class LongitudinalMpc(object):
       self.save_car_data()
       TR = self.smooth_follow()
       cost = self.get_cost(TR)
-      cost = 0.8
+      cost = 0.1
       TR = 1.8
       if abs(cost - self.last_cost) > .15:
         self.libmpc.init(MPC_COST_LONG.TTC, cost, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
