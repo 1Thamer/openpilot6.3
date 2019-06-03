ea5340e7b85c42b7774d94897366ae7c74965fe3
Arne Schwarck <arneschwarck@gmail.com>
2019-06-03 18:21:54 +0200

    constantly change cost

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -163,9 +163,8 @@ class LongitudinalMpc(object):
       self.save_car_data()
       TR = self.smooth_follow()
       cost = self.get_cost(TR)
-      if abs(cost - self.last_cost) > .15:
-        self.libmpc.init(MPC_COST_LONG.TTC, cost, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
-        self.last_cost = cost
+      self.libmpc.init(MPC_COST_LONG.TTC, cost, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
+      self.last_cost = cost
       return TR
     else:
       if self.last_cost != 0.05:
