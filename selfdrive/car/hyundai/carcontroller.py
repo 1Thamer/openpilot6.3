5ee6fce1175101a32afeb225a633acec1f32d6b7
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-07-12 16:06:27 +0300

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -56,11 +56,10 @@ class CarController(object):
         can_sends.append(create_1191())
       if (self.cnt % 7) == 0:
         can_sends.append(create_1156())
-
+    else:
+      can_sends.append(create_mdps12(self.packer, self.car_fingerprint, self.mdps12_cnt, CS.mdps12, CS.lkas11))
     can_sends.append(create_lkas11(self.packer, self.car_fingerprint, apply_steer, steer_req, self.lkas11_cnt,
                                    enabled, CS.lkas11, hud_alert, keep_stock=(not self.camera_disconnected)))
-    
-    can_sends.append(create_mdps12(self.packer, self.car_fingerprint, self.mdps12_cnt, CS.mdps12, CS.lkas11))
 
     #if pcm_cancel_cmd:
       #can_sends.append(create_clu11(self.packer, CS.clu11, Buttons.CANCEL))
