46551baabd95cc12cefb1555d3a64927a26e9121
Andrew Frahn <andrew@emmertex.com>
2019-07-12 15:24:48 +1000

    bug

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -178,7 +178,7 @@ class CarInterface(object):
     ret.seatbeltUnlatched = not self.CS.seatbelt
 
     # low speed steer alert hysteresis logic (only for cars with steer cut off above 10 m/s)
-    if self.CS.v_ego_raw < (self.CS.min_steer_speed + 0.1) and self.CS.min_steer_speed > 2.:
+    if self.CS.v_ego_raw < (self.CS.min_steer_speed) and self.CS.min_steer_speed > 2.:
       self.low_speed_alert = True
     if self.CS.v_ego_raw > (self.CS.min_steer_speed + 0.2):
       self.low_speed_alert = False
