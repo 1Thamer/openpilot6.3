d851311a2a603942671bfd57c073d6a756e0ea4f
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-07-30 18:24:11 +0300

    relax steer limit

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -1,8 +1,8 @@
-const int HYUNDAI_MAX_STEER = 300;             // like stock
-const int HYUNDAI_MAX_RT_DELTA = 112;          // max delta torque allowed for real time checks
+const int HYUNDAI_MAX_STEER = 500;             // like stock
+const int HYUNDAI_MAX_RT_DELTA = 350;          // max delta torque allowed for real time checks
 const uint32_t HYUNDAI_RT_INTERVAL = 250000;    // 250ms between real time checks
-const int HYUNDAI_MAX_RATE_UP = 6;
-const int HYUNDAI_MAX_RATE_DOWN = 8;
+const int HYUNDAI_MAX_RATE_UP = 15;
+const int HYUNDAI_MAX_RATE_DOWN = 35;
 const int HYUNDAI_DRIVER_TORQUE_ALLOWANCE = 50;
 const int HYUNDAI_DRIVER_TORQUE_FACTOR = 2;
 
