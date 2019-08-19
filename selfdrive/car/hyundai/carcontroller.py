dba0780bd7163453d939fd5c0cc07fc751d5f626
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-08-19 20:27:13 +0300

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -9,9 +9,9 @@ from selfdrive.can.packer import CANPacker
 # Steer torque limits
 
 class SteerLimitParams:
-  STEER_MAX = 500   # 409 is the max, 255 is stock
-  STEER_DELTA_UP = 10
-  STEER_DELTA_DOWN = 15
+  STEER_MAX = 409   # 409 is the max, 255 is stock
+  STEER_DELTA_UP = 7
+  STEER_DELTA_DOWN = 9
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
