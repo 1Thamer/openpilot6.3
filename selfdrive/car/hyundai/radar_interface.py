4f7c58a7acbb700c1d991486f84e5d9b45c69d0b
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-08-20 18:49:56 +0300

    Update radar_interface.py

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
-from cereal import car
 import time
+from cereal import car
 from selfdrive.can.parser import CANParser
-from selfdrive.car.hyundai.carstate import CarState, get_can_parser
+from selfdrive.car.hyundai.values import DBC
 from common.realtime import sec_since_boot
+
+def get_radar_can_parser(CP):
+
+  signals = [
+    # sig_name, sig_address, default
+    ("ACC_ObjStatus", "SCC11", 0),
+    ("ACC_ObjLatPos", "SCC11", 0),
+    ("ACC_ObjDist", "SCC11", 0),
+    ("ACC_ObjRelSpd", "SCC11", 0),
+  ]
+
+  checks = [
+    # address, frequency
+    ("SCC11", 50),
+  ]
+
+  return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 0)
 
 class RadarInterface(object):
   def __init__(self, CP):
     # radar
     self.pts = {}
     self.delay = 0.1
-    self.CS = CarState(CP)
-    self.cp = get_can_parser(CP)
+    self.rcp = get_radar_can_parser(CP)
     self.updated_messages = set()
     self.no_radar = False
 
@@ -22,7 +38,7 @@ class RadarInterface(object):
       return ret
 
     tm = int(sec_since_boot() * 1e9)
-    vls = self.cp.update_strings(tm, can_strings)
+    vls = self.rcp.update_strings(tm, can_strings)
     self.updated_messages.update(vls)
 
     rr =  self._update(self.updated_messages)
@@ -31,7 +47,6 @@ class RadarInterface(object):
     return rr
   
   
-
   def _update(self, updated_messages):
     ret = car.RadarData.new_message()
     cpt = self.rcp.vl
@@ -40,11 +55,11 @@ class RadarInterface(object):
       errors.append("canError")
     ret.errors = errors
 
-    valid = self.CS.scc11["ACC_ObjStatus"]
+    valid = cpt["SCC11"]['ACC_ObjStatus']
     if valid:
-      self.pts.dRel = self.CS.scc11["ACC_ObjDist"]  # from front of car
-      self.pts.yRel = self.CS.scc11["ACC_ObjLatPos"]  # in car frame's y axis, left is negative
-      self.pts.vRel = self.CS.scc11["ACC_ObjRelSpd"]
+      self.pts.dRel = cpt["SCC11"]['ACC_ObjDist']  # from front of car
+      self.pts.yRel = cpt["SCC11"]['ACC_ObjLatPos']  # in car frame's y axis, left is negative
+      self.pts.vRel = cpt["SCC11"]['ACC_ObjRelSpd']
       self.pts.aRel = float('nan')
       self.pts.yvRel = float('nan')
       self.pts.measured = True
